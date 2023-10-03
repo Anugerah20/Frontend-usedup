c51dcf0d2fea2792e027d79e4a06f4714492afbd
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-10-03 22:24:13 +0700

    add new file

---
+++ src/pages/Index.jsx
@@ -0,0 +1,11 @@
+// Nama file bisa direname aja
+
+const Index = () => {
+     return (
+          <>
+               <h1>Pages Index sementara</h1>
+          </>
+     )
+}
+
+export default Index
