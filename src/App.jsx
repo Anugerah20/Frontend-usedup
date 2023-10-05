f36cacb925c06320129c485ad0334162956deebe
adamrayw <raywibowo68@gmail.com>
2023-10-05 23:00:05 +0700

    feature: install and setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import EditProfile from './pages/EditProfile'  
-import Index from "./pages/Index";
+import EditProfile from './pages/EditProfile'
 import LoginCard from "./components/LoginCard";
 import RegisterCard from "./components/RegisterCard";
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <>
-      <div>
-        <EditProfile />
-     </div>
-//       <div className="flex">
-//         <LoginCard />
-//         <RegisterCard />
-//       </div>
-    </>
+    <div className='App flex justify-start items-center h-screen'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginCard />} />
+        <Route path='/register' element={<RegisterCard />} />
+        <Route path='/edit-profile' element={<EditProfile />} />
+      </Routes>
+    </div>
   );
 }
 
