0f7eb1276a1a1b6a4e638e03cefa55fc26b22a18
adamrayw <raywibowo68@gmail.com>
2023-10-07 20:17:38 +0700

    show banner on main page,

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import EditProfile from './pages/EditProfile'
 import LoginCard from "./components/LoginCard";
 import RegisterCard from "./components/RegisterCard";
 import Navigation from "./components/Navigation";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { Home } from './pages/Home';
+import Banner from './assets/banner.webp'
 
 function App() {
+
+  const pathname = useLocation().pathname
+
   return (
     <>
       <Navigation />
+
+      {/* Tampilkan banner hanya di halaman utama */}
+      <img src={Banner} alt='banner' className={`${pathname === '/' ? 'block' : 'hidden'}`} />
+
       <div className='App px-4 sm:my-10 my-5'>
         <Routes>
           <Route path='/' element={<Home />} />
