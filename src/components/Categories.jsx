226c47de4df50e5dbbfcae35cc11e54f402c201e
adamrayw <raywibowo68@gmail.com>
2023-10-09 21:01:03 +0700

    add underline when hover

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,11 +4,11 @@ const Categories = () => {
         <div className="categories shadow-sm">
             <div className="group-categories ">
                 <ul className="flex gap-4 text-sm sm:pl-0 pl-4 text-primary">
-                    <li className="mb-5 mr-0"><a href="#">Mobil Bekas</a></li>
-                    <li className="mb-5 mr-0"><a href="#">Motor Bekas</a></li>
-                    <li className="mb-5 mr-0"><a href="#">Property</a></li>
-                    <li className="mb-5 mr-0"><a href="#">Handphone</a></li>
-                    <li className="mb-5 mr-0"><a href="#">TV & Audio, Video</a></li>
+                    <li className="mb-5 mr-0 hover:underline"><a href="#">Mobil Bekas</a></li>
+                    <li className="mb-5 mr-0 hover:underline"><a href="#">Motor Bekas</a></li>
+                    <li className="mb-5 mr-0 hover:underline"><a href="#">Property</a></li>
+                    <li className="mb-5 mr-0 hover:underline"><a href="#">Handphone</a></li>
+                    <li className="mb-5 mr-0 hover:underline"><a href="#">TV & Audio, Video</a></li>
                 </ul>
             </div>
         </div>
