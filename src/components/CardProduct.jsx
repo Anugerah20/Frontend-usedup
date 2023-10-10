d234c40db6b3356387b709b223cd8e1f332593e0
adamrayw <raywibowo68@gmail.com>
2023-10-10 23:26:33 +0700

    remove border

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -6,7 +6,7 @@ const CardProduct = ({ name, image, price, location }) => {
     const [isFavorite, setIsFavorite] = useState(false);
 
     return (
-        <div className="border rounded overflow-hidden relative">
+        <div className="card rounded overflow-hidden relative">
             <img src={image} alt={name} className='max-w-full object-contain mx-auto' />
             <div className="px-4 md:px-6 py-4">
                 <div className="font-bold text-base md:text-lg mb-1 line-clamp-1">{name}</div>
