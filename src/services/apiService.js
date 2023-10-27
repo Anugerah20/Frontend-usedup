3c7b4e70414946dfb7360360c1b385dcdf0d7fb7
adamrayw <raywibowo68@gmail.com>
2023-10-28 02:16:02 +0700

    remove timeout

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const apiService = axios.create({
      baseURL: "http://localhost:3000/api",
-     timeout: 1000,
 });
 
 export const useApiPost = async (url, userData) => {
