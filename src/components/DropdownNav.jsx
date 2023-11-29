6b68d78b34dd62ccfbb78dd2349a8205a998db49
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-11-29 17:03:07 +0700

    add useEffect data user

--- src/components/DropdownNav.jsx
+++ src/components/DropdownNav.jsx
@@ -1,21 +1,25 @@
 import ProfileUser from "../assets/profile-user.png"
 import { Avatar, Dropdown } from "flowbite-react"
+import { useEffect, useState } from "react";
 import { FaBullhorn, FaHeart, FaSignOutAlt } from "react-icons/fa";
-import { useNavigate } from "react-router-dom"
+import { useApiGet, userLogout } from "../services/apiService";
 const DropdownNav = () => {
-     const navigation = useNavigate();
+     const [userData, setUserData] = useState(null);
 
-     const userLogout = () => {
-          try {
-               localStorage.removeItem("useToken")
-               console.log("remove token")
-               setTimeout(() => {
-                    navigation("/login")
-               }, 100)
-          } catch (error) {
-               console.error("Error logout", error);
+     // Get Data User
+     useEffect(() => {
+          const fetchUser = async () => {
+               try {
+                    const userId = localStorage.getItem("userId");
+                    const res = await useApiGet(`/user/${userId}`);
+                    setUserData(res.data);
+               } catch (error) {
+                    console.log(error);
+               }
           }
-     }
+          fetchUser();
+     }, []);
+
      return (
           <div className="flex">
                <Dropdown
@@ -29,7 +33,7 @@ const DropdownNav = () => {
                          <div className="flex items-center my-2 space-x-2">
                               <img src={ProfileUser} alt="Profile User" className="w-12 h-12 rounded-full" />
                               <span className="text-sm text-primary mt-2 ml-2">
-                                   Halo, <p className="font-bold py-1 text-primary">Nabil Anugerah</p>
+                                   <p className="py-1 text-primary"> Halo, <span className="font-bold">{userData?.fullname}</span></p>
                                    <a href="#" className="underline text-secondary font-normal">Edit Profile</a>
                               </span>
                          </div>
