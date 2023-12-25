0994e277a24b871b4bc05fccc33d4ac0713ff942
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-12-25 12:04:01 +0700

    feat: show data to card product backend

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -2,14 +2,14 @@
 import { useState } from "react";
 import { AiFillHeart } from "react-icons/ai";
 
-const CardProduct = ({ name, image, price, location }) => {
+const CardProduct = ({ title, image, price, location }) => {
     const [isFavorite, setIsFavorite] = useState(false);
 
     return (
         <div className="card rounded overflow-hidden relative">
-            <img src={image} alt={name} className='max-w-full object-contain mx-auto' />
+            <img src={image} alt={title} className='max-w-full object-contain mx-auto' />
             <div className="px-4 md:px-6 py-4">
-                <div className="font-bold text-base md:text-lg mb-1 line-clamp-1">{name}</div>
+                <div className="font-bold text-base md:text-lg mb-1 line-clamp-1">{title}</div>
                 <p className="text-blue-link font-bold text-base md:text-xl">{price}</p>
                 <p className="text-secondary text-right mt-4 text-xs uppercase md:text-sm">{location}</p>
             </div>
