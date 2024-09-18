2a753040d0bee0dae5a1ea2feeb0d4e30cd92c57
Nabil Anugerah <nabilpangestu123@gmail.com>
2024-09-18 08:46:07 +0700

    Refactor code to improve map functionality and user experience

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
@@ -6,16 +6,39 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const Map = () => {
-     const mapContainerRef = useState(null); // kontainer peta
-     const mapRef = useState(null); // Instance peta
-     const markerRef = useState(null); // Untuk marker
-     const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_TOKEN_MAPBOX; // Token Mapbox
+     const mapContainerRef = useRef(null);
+     const mapRef = useRef(null);
+     const markerRef = useRef(null);
+     const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_TOKEN_MAPBOX;
 
-     // State detail alamat
+     // State detail alamat dan lokasi user
      const [address, setAddress] = useState('');
 
-     useEffect(() => {
+     // State lokasi user saat ini
+     const [currentLocation, setCurrentLocation] = useState(null);
 
+     // Fungsi untuk reverse geocoding
+     const fetchAddress = async (lng, lat) => {
+          const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOX_ACCESS_TOKEN}`);
+          const data = await response.json();
+          if (data?.features?.length) {
+               return data.features[0].place_name;
+          }
+          return 'Address not found';
+     };
+
+     // Fungsi untuk membuat marker baru
+     const createMarker = (lng, lat) => {
+          if (markerRef.current) {
+               markerRef.current.remove();
+          }
+          const marker = new mapboxgl.Marker()
+               .setLngLat([lng, lat])
+               .addTo(mapRef.current);
+          markerRef.current = marker;
+     };
+
+     useEffect(() => {
           // Set token Mapbox
           mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
 
@@ -23,91 +46,64 @@ const Map = () => {
           mapRef.current = new mapboxgl.Map({
                container: mapContainerRef.current,
                style: 'mapbox://styles/mapbox/streets-v12',
-               center: [106.827183, -6.175394], // Koordinat awal peta (longitude, latitude)
                zoom: 13
           });
 
-          // Inisialisasi Geocoder (untuk pencarian lokasi)
+          // Fungsi untuk mendapatkan lokasi user saat ini
+          const getUserLocation = () => {
+               if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(async (position) => {
+                         const { latitude, longitude } = position.coords;
+                         setCurrentLocation([longitude, latitude]);
+
+                         // Arahkan peta dan buatkan marker
+                         mapRef.current.flyTo({ center: [longitude, latitude], zoom: 14 });
+                         createMarker(longitude, latitude);
+
+                         // detail alamat user
+                         const address = await fetchAddress(longitude, latitude);
+                         setAddress(address);
+                    }, (error) => {
+                         console.error('Error getting user location:', error);
+                    });
+               } else {
+                    console.error('Geolocation is not supported by this browser');
+               }
+          };
+
+          // fungsi untuk mendapatkan lokasi user
+          getUserLocation();
+
+          // Inisialisasi Geocoder
           const geocoder = new MapboxGeocoder({
                accessToken: mapboxgl.accessToken,
                mapboxgl: mapboxgl,
-               marker: false // Menonaktifkan marker bawaan dari Geocoder
+               marker: false // Nonaktifkan marker default dari Geocoder
           });
 
           // Menambahkan Geocoder ke peta
           mapRef.current.addControl(geocoder);
 
-          // Event listener ketika lokasi dipilih dari hasil pencarian Geocoder
-          geocoder.on('result', (e) => {
-               const { geometry } = e.result;
-
-               // Jika marker sudah ada hapus marker sebelumnya
-               if (markerRef.current) {
-                    markerRef.current.remove();
-               }
-
-               // Tambahkan marker di lokasi yang dipilih
-               const marker = new mapboxgl.Marker()
-                    .setLngLat(geometry.coordinates) // Koordinat dari hasil pencarian
-                    .addTo(mapRef.current); // Tambahkan marker ke peta
-
-               markerRef.current = marker;
-
-               // Arahkan peta ke lokasi yang dipilih
-               mapRef.current.flyTo({
-                    center: geometry.coordinates,
-                    zoom: 14
-               });
-
-
-               // Reset address state
-               setAddress('');
-
-               // Debugging: output koordinat ke console
-               // console.log('Koordinat dari hasil pencarian: ', geometry.coordinates);
+          // Event ketika lokasi dipilih dari hasil pencarian Geocoder
+          geocoder.on('result', async (e) => {
+               const { coordinates } = e.result.geometry;
+               createMarker(coordinates[0], coordinates[1]);
+               mapRef.current.flyTo({ center: coordinates, zoom: 14 });
+               setAddress(e.result.place_name);
           });
 
-          // Trigger ketika pengguna mengklik di peta
+          // Event ketika user mengklik di peta
           mapRef.current.on('click', async (e) => {
                const { lng, lat } = e.lngLat;
+               createMarker(lng, lat);
+               mapRef.current.flyTo({ center: [lng, lat], zoom: 14 });
 
-               // Jika marker sudah ada, hapus marker sebelumnya
-               if (markerRef.current) {
-                    markerRef.current.remove();
-               }
-
-               // Tambahkan marker di lokasi yang diklik
-               const marker = new mapboxgl.Marker()
-                    .setLngLat([lng, lat]) // Set marker ke posisi klik
-                    .addTo(mapRef.current); // Tambahkan marker ke peta
-
-               markerRef.current = marker;
-
-               // Arahkan peta ke lokasi yang diklik (opsional)
-               mapRef.current.flyTo({
-                    center: [lng, lat],
-                    zoom: 14
-               });
-
-               // Call reverse geocoding API to get address
-               const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOX_ACCESS_TOKEN}`);
-
-               const data = await response.json();
-
-               // Debugging: output koordinat lnt & lat
-               // console.log('Koordinat dari lng & lat: ', [lng, lat]);
-
-               // Conditional statement untuk mengambil detail alamat
-               // Extract address from the API response
-               if (data && data.features && data.features.length > 0) {
-                    setAddress(data.features[0].place_name);
-               } else {
-                    setAddress('Address not found');
-               }
-
+               // Ambil detail alamat
+               const address = await fetchAddress(lng, lat);
+               setAddress(address);
           });
 
-          // Clean up saat komponen dibongkar
+          // Cleanup saat komponen dibongkar
           return () => {
                if (mapRef.current) {
                     mapRef.current.remove();
@@ -119,16 +115,29 @@ const Map = () => {
           <div>
                <div ref={mapContainerRef} className="w-full h-56"></div>
 
-               {/* Display the address */}
+               {/* Menampilkan detail alamat user */}
                {address && (
                     <div className="mt-4">
-                         <h4 className="font-bold">Detail Alamat:</h4>
+                         <h4 className="font-bold">Detail Lokasi:</h4>
                          <p>{address}</p>
                     </div>
                )}
+
+
+               {/*
+                    EDITOR: NABIL
+                    DESCRIPTION: melihat longitude dan latitude user, boleh dihapus karena cuma untuk debugging
+               */}
+
+               {/* {currentLocation && (
+                    <div className="mt-4">
+                         <h4 className="font-bold">Lokasi Saat Ini:</h4>
+                         <p>Longitude: {currentLocation[0]}</p>
+                         <p>Latitude: {currentLocation[1]}</p>
+                    </div>
+               )} */}
           </div>
      );
-
 };
 
 export default Map;
