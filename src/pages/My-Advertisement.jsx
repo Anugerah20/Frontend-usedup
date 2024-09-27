2de3833f498ef06ae5c695dbe4dfff7ffff1f79d
Adam Ray <raywibowo68@gmail.com>
2024-09-27 23:16:01 +0700

    Refactor MyAdvertisement component to include loading state

--- src/pages/My-Advertisement.jsx
+++ src/pages/My-Advertisement.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { useApiGet, useApiDelete } from '../services/apiService';
 import { toastSuccess } from '../services/toatsService';
@@ -7,6 +7,7 @@ import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 const MyAdvertisement = () => {
      const [dataAdvert, setDataAdvert] = useState([]);
+     const [loading, setLoading] = useState(false);
      const [openModal, setOpenModal] = useState(false);
      const [deleteItemId, setDeleteItemId] = useState(null);
      const userId = localStorage.getItem("userId");
@@ -14,14 +15,15 @@ const MyAdvertisement = () => {
      // Menampilkan Iklan yang dibuat oleh user
      const getDataAdvert = async () => {
           try {
+               setLoading(true);
                const getData = await useApiGet(`/user/${userId}`);
                setDataAdvert(getData.data.advert);
+               setLoading(false);
           } catch (error) {
+               setLoading(false);
                console.log("Show Data Advert", error);
           }
      }
-
-     console.log(dataAdvert)
 
      useEffect(() => {
           getDataAdvert();
@@ -69,29 +71,65 @@ const MyAdvertisement = () => {
                <p className="text-sm text-gray-breadcrumb-secondary">Dibawah ini merupakan iklan yang tersedia</p>
                <hr className="w-[295px] mt-3" />
 
-               {dataAdvert.map((item, index) => (
-                    <div key={item.id} className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 my-5">
-                         <div className="flex gap-10">
-                              <div className="text-sm font-bold text-primary">
-                                   {index + 1}.
-                              </div>
-                              <img src={item.image[0]} alt="Mobil" className="w-60 h-32 md:w-48 md:h-32 xl:w-60 xl:h-40 object-cover rounded row-start-2" />
-                         </div>
-                         <div className="ml-12 md:ml-5 lg:ml-0">
-                              <h5 className="text-primary text-xl font-bold">{item.title}</h5>
-                              <p className="text-sm mt-2 text-gray-breadcrumb">
-                                   ID Iklan : {item.id}
-                              </p>
-                              <div className="flex flex-wrap my-5 gap-4">
-                                   <Link to={`/detail/${item.id}`} className="text-md text-blue-link underline">Lihat Iklan</Link>
-                                   {/* <p className="text-md text-gray-breadcrumb">Dilihat 10 Kali</p> */}
+               {loading ? (
+                    <Fragment>
+                         <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4 my-5">
+                              <div className="flex gap-10">
+                                   <div className="text-sm font-bold text-primary">
+                                        <div className="h-6 w-6 bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
+                                   <div className="w-60 h-32 md:w-48 md:h-32 xl:w-60 xl:h-40 object-cover rounded ">
+                                        <div className="h-full w-full bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
                               </div>
                          </div>
-                         <div className="ml-12 md:ml-10 lg:ml-0 flex sm:justify-start md:justify-end lg:justify-end">
-                              <button onClick={() => openDeleteModal(item.id)} className="flex justify-center items-center text-sm w-28 h-10 bg-red-500 hover:bg-red-600 text-white transition rounded">Hapus Iklan</button>
+                         <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4 my-5">
+                              <div className="flex gap-10">
+                                   <div className="text-sm font-bold text-primary">
+                                        <div className="h-6 w-6 bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
+                                   <div className="w-60 h-32 md:w-48 md:h-32 xl:w-60 xl:h-40 object-cover rounded ">
+                                        <div className="h-full w-full bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
+                              </div>
                          </div>
-                    </div>
-               ))}
+                         <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4 my-5">
+                              <div className="flex gap-10">
+                                   <div className="text-sm font-bold text-primary">
+                                        <div className="h-6 w-6 bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
+                                   <div className="w-60 h-32 md:w-48 md:h-32 xl:w-60 xl:h-40 object-cover rounded ">
+                                        <div className="h-full w-full bg-gray-200 rounded-lg animate-pulse"></div>
+                                   </div>
+                              </div>
+                         </div>
+                    </Fragment>
+               ) :
+                    (
+                         dataAdvert.map((item, index) => (
+                              <div key={item.id} className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 my-5">
+                                   <div className="flex gap-10">
+                                        <div className="text-sm font-bold text-primary">
+                                             {index + 1}.
+                                        </div>
+                                        <img src={item.image[0]} alt="Mobil" className="w-60 h-32 md:w-48 md:h-32 xl:w-60 xl:h-40 object-cover rounded row-start-2" />
+                                   </div>
+                                   <div className="ml-12 md:ml-5 lg:ml-0">
+                                        <h5 className="text-primary text-xl font-bold">{item.title}</h5>
+                                        <p className="text-sm mt-2 text-gray-breadcrumb">
+                                             ID Iklan : {item.id}
+                                        </p>
+                                        <div className="flex flex-wrap my-5 gap-4">
+                                             <Link to={`/detail/${item.id}`} className="text-md text-blue-link underline">Lihat Iklan</Link>
+                                             {/* <p className="text-md text-gray-breadcrumb">Dilihat 10 Kali</p> */}
+                                        </div>
+                                   </div>
+                                   <div className="ml-12 md:ml-10 lg:ml-0 flex sm:justify-start md:justify-end lg:justify-end">
+                                        <button onClick={() => openDeleteModal(item.id)} className="flex justify-center items-center text-sm w-28 h-10 bg-red-500 hover:bg-red-600 text-white transition rounded">Hapus Iklan</button>
+                                   </div>
+                              </div>
+                         ))
+                    )}
 
                {/* Modal Konfirmasi Hapus Iklan */}
                <Modal show={openModal} size="md" onClose={closeDeleteModal} popup>
